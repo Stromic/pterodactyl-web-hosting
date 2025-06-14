3c1bb692e48f169d33f50c006c6ad70d42c81e99
stromic <stromic@protonmail.com>
2025-06-14 15:37:21 +0200

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sleep 1
+
+cd /home/container
+
+# Copy defaults if /home/container/apache2/httpd.conf is empty we copy over the default configs
+if [ ! -d /home/container/apache2 ]; then
+    echo "[Entrypoint] Copying default files..."
+    cp -r /usr/local/default_files/apache2 /home/container
+fi
+
+if [ ! -d /home/container/php-fpm ]; then
+    echo "[Entrypoint] Copying default files..."
+    cp -r /usr/local/default_files/php-fpm /home/container
+fi
+
+if [ ! -d /home/container/public_html ]; then
+    echo "[Entrypoint] Copying default files..."
+    cp -r /usr/local/default_files/public_html /home/container
+fi
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
+
+echo "Starting PHP-FPM..."
+/usr/sbin/php-fpm83 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+
+echo "Starting Apache..."
+httpd -f /home/container/apache2/httpd.conf -DFOREGROUND
